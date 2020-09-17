39254e5967615929583feb768339746d5dc19f14
venkata Bonda <bonda.selenium@gmail.com>
2020-09-17 16:36:50 +0100

    new tests added

--- cypress/integration/examples/DragAndDrop.js
+++ cypress/integration/examples/DragAndDrop.js
@@ -5,8 +5,7 @@ describe('Drag and drop', () => {
         
       cy.visit('https://www.seleniumeasy.com/test/drag-and-drop-demo.html')
       cy.get('#todrag>span:nth-child(2)').drag('#mydropzone')
-      cy.get('#droppedlist > span').should('eq','Draggable 1')
-      //.should('contain,text','Draggable 1')
+
     })
 
 })
