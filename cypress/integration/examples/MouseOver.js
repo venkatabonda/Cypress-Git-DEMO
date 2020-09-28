df77202a4d032dc71c2c9988bbdfacf78a697f8d
venkata Bonda <bonda.selenium@gmail.com>
2020-09-28 14:49:18 +0100

    added background colour tests

--- cypress/integration/examples/MouseOver.js
+++ cypress/integration/examples/MouseOver.js
@@ -1,8 +1,15 @@
 /// <reference types ="cypress"  />
 
+<<<<<<< HEAD
 describe('Click on signup test', () => {
 
     it('Verify file upload functionality ', () => {
+=======
+
+describe('Click on signup test', () => {
+
+    it.skip('Verify file upload functionality ', () => {
+>>>>>>> 51ecdea37542f40591c578c6bc2ef2d5707d7cf6
         
       cy.visit('https://www.spicejet.com//')
       cy.injectAxe();
@@ -14,15 +21,30 @@ describe('Click on signup test', () => {
              })
 
 
+<<<<<<< HEAD
    it('Verify file upload functionality ', () => {
         
          cy.visit('http://automationpractice.com/index.php')
+=======
+   it('Verify file upload functionality', () => {
+        
+         cy.visit('http://automationpractice.com/index.php')
+         //cy.injectAxe();
+>>>>>>> 51ecdea37542f40591c578c6bc2ef2d5707d7cf6
                 cy.title().should('eq','My Store')
                 cy.get(".ajax_add_to_cart_button").first().click()
                 cy.get(".cross").click()
                 cy.wait(3000)
                 cy.contains("Cart").trigger("#button_order_cart").click()
+<<<<<<< HEAD
                  cy.url().should('include','controller=order')
              
               })
 })
+=======
+                cy.url().should('include','?controller=order')
+                 //cy.checkA11y();
+              })
+
+            })
+>>>>>>> 51ecdea37542f40591c578c6bc2ef2d5707d7cf6
